09a315ecd061195d2a2ce7a39ac3796695317668
razvanrosa <razvan_rosa@yahoo.com>
2020-08-06 19:14:15 +0300

    udded update task endpoint

--- src/main/java/org/fasttrackit/web/TaskServlet.java
+++ src/main/java/org/fasttrackit/web/TaskServlet.java
@@ -4,6 +4,7 @@ import org.fasttrackit.config.ObjectMapperConfiguration;
 import org.fasttrackit.domain.Task;
 import org.fasttrackit.service.TaskService;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -42,6 +43,19 @@ public class TaskServlet extends HttpServlet {
 
             ObjectMapperConfiguration.getObjectMapper().writeValue(resp.getWriter(), tasks);
 
+        } catch (SQLException e) {
+            resp.sendError(500, e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idAsString = req.getParameter("id");
+        UpdateTaskRequest request = ObjectMapperConfiguration.getObjectMapper().
+                readValue(req.getReader(), UpdateTaskRequest.class);
+
+        try {
+            taskService.updateTask(Long.parseLong(idAsString),request);
         } catch (SQLException e) {
             resp.sendError(500, e.getMessage());
         }
